ce2b592d2fd5cb88cb679509a91b296c8cfccc42
Viczinha007 <115582531+Viczinha007@users.noreply.github.com>
2022-10-11 15:59:48 -0300

    Add files via upload

---
+++ formesqueciSenha.java
@@ -0,0 +1,16 @@
+package com.example.apptcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class formesqueciSenha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formesqueci_senha);
+
+        getSupportActionBar().hide();
+    }
+}
