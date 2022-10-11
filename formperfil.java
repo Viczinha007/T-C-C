3744e7c4065301c223c6332be4074afc4354a89c
Viczinha007 <115582531+Viczinha007@users.noreply.github.com>
2022-10-11 16:00:32 -0300

    Add files via upload

---
+++ formperfil.java
@@ -0,0 +1,14 @@
+package com.example.apptcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class formperfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formperfil);
+    }
+}
